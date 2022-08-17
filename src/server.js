f46eb56df457771d290222c114ed98d1b0fa22cb
Amoong <moo1323@naver.com>
2022-08-17 21:13:00 +0900

    Recap

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import res from "express/lib/response";
 
 const app = express();
 
@@ -6,6 +7,7 @@ app.set("view engine", "pug");
 app.set("views", `${__dirname}/views`);
 app.use("/static", express.static(`${__dirname}/static`));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
