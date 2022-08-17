b6a47ee274af32245bbc961e4d576677a4b5824d
Amoong <moo1323@naver.com>
2022-08-17 21:51:36 +0900

    WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
-import res from "express/lib/response";
 
 const app = express();
 
@@ -10,4 +11,11 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+// server 전달은 필수가 아님, http와 wss 서버를 동시에 돌리고 싶을 때 사용
+// http 서버 위에 wss 서버가 돌아간다.
+// http 서버를 돌리는 이유는 GET 요청 같은걸 처리하기 위해서
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
