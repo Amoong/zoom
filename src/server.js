58bce16c0c403adedbb5eb114ea909b1a5cf471f
Amoong <moo1323@naver.com>
2022-09-03 08:38:19 +0900

    Admin Panel

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
 import express from "express";
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -13,7 +14,16 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+});
 
 function publicRooms() {
   const {
@@ -33,7 +43,6 @@ function publicRooms() {
 }
 
 function countRoom(roomName) {
-  console.log(wsServer.sockets.adapter);
   return wsServer.sockets.adapter.rooms.get(roomName)?.size;
 }
 
